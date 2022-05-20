e29d54ceea85cfbeceb5376712bdd3d4b3a0a1fd
anlgnc <66387811+anlgnc@users.noreply.github.com>
2022-05-20 15:18:16 +0300

    Add files via upload

---
+++ cleaner.php
@@ -0,0 +1,27 @@
+<?php
+/*
+/* Nirvana Yazılım
+/* 20.05.2022 - 11:44
+/* anil.gonca@nirvanayazilim.com
+/* Table Clear for ci_sessions
+*/
+date_default_timezone_set('Europe/Istanbul');
+        //DB Config
+        $hostName = "localhost";
+        $dbName = "test";
+        $dbUserName = "root";
+        $dbUserPassword = "";
+        try {
+            $conn = new PDO("mysql:host=$hostName;dbname=$dbName", $dbUserName, $dbUserPassword);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+        $now = new DateTime();
+        $now->sub(new \DateInterval('PT7200S'));
+        $time = $now->getTimestamp();
+        $sql = 'DELETE FROM `ci_sessions` WHERE `timestamp` <= :tmp';
+        $sqlQuery = $conn->prepare($sql);
+        $sqlQuery->bindParam("tmp", $time, PDO::FETCH_ASSOC);
+        $sqlQuery->execute();
+?>
